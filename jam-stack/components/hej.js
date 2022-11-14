b23b3e3118086f984ac6bef35b5b06dc16561d1a
Andrea-Agosta <andrea.agosta85@gmail.com>
2022-11-14 14:28:25 +0100

    initial setup

---
+++ jam-stack/components/hej.js
@@ -0,0 +1,4 @@
+
+const Hej = () => <h1>SALAMON IS HERE</h1>;
+
+export default Hej;
---
+++ jam-stack/pages/monika.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hej from '../components/hej'
+
+const monika = () => {
+  return (
+    <></>
+  )
+}
+
+export default monika
